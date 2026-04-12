668f4a5d09e69b6ff346e5afea5b04da552345dc
David <daviddenisson2014@gmail.com>
2026-04-12 00:49:53 -0300

    feat: migrate player cards to Tailwind + stagger entrance animation

--- src/app/components/cards-players-service/cards-players-service.component.ts
+++ src/app/components/cards-players-service/cards-players-service.component.ts
@@ -1,29 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  trigger,
+  transition,
+  style,
+  animate,
+  query,
+  stagger,
+} from '@angular/animations';
 import { CardsPlayersServiceService } from 'src/app/services/cards-players-service.service';
 
 @Component({
   selector: 'app-cards-players-service',
   templateUrl: './cards-players-service.component.html',
-  styleUrls: ['./cards-players-service.component.css']
+  styleUrls: ['./cards-players-service.component.css'],
+  animations: [
+    trigger('listAnimation', [
+      transition('* => *', [
+        query(
+          ':enter',
+          [
+            style({ opacity: 0, transform: 'translateY(24px)' }),
+            stagger(30, [
+              animate(
+                '400ms cubic-bezier(0.16, 1, 0.3, 1)',
+                style({ opacity: 1, transform: 'translateY(0)' })
+              ),
+            ]),
+          ],
+          { optional: true }
+        ),
+      ]),
+    ]),
+  ],
 })
-export class CardsPlayersServiceComponent {
+export class CardsPlayersServiceComponent implements OnInit {
 
-  
-  cards:any;
+  cards: any[] = [];
 
-  constructor(private cardsService: CardsPlayersServiceService){
+  constructor(private cardsService: CardsPlayersServiceService) {}
 
+  ngOnInit(): void {
+    this.cardsService.getCards().subscribe((Response: any) => {
+      this.cards = Response.data.filter(
+        (card: any) => card.largeArt != null && card.largeArt !== ''
+      );
+    });
   }
-
-  ngOnInit(){
-
-    this.cardsService.getCards().subscribe((Response : any) => {
-      this.cards = Response.data;
-
-      this.cards.filter((card:any) => card.largeArt != null && card.largeArt != '');
-
-    })
-
-  }
-
 }

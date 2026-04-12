88d9129da3a981475a1531ab12024a8e6c43fbd1
David <daviddenisson2014@gmail.com>
2026-04-12 00:49:13 -0300

    feat: migrate temporadas to Tailwind + stagger animation

--- src/app/components/temporadas-component/temporadas-component.component.ts
+++ src/app/components/temporadas-component/temporadas-component.component.ts
@@ -1,37 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  trigger,
+  transition,
+  style,
+  animate,
+  query,
+  stagger,
+} from '@angular/animations';
 import { TemporadasServicesService } from 'src/app/services/temporadas-services.service';
 
 @Component({
   selector: 'app-temporadas-component',
   templateUrl: './temporadas-component.component.html',
-  styleUrls: ['./temporadas-component.component.css']
+  styleUrls: ['./temporadas-component.component.css'],
+  animations: [
+    trigger('listAnimation', [
+      transition('* => *', [
+        query(
+          ':enter',
+          [
+            style({ opacity: 0, transform: 'translateY(24px)' }),
+            stagger(60, [
+              animate(
+                '350ms cubic-bezier(0.16, 1, 0.3, 1)',
+                style({ opacity: 1, transform: 'translateY(0)' })
+              ),
+            ]),
+          ],
+          { optional: true }
+        ),
+      ]),
+    ]),
+  ],
 })
+export class TemporadasComponentComponent implements OnInit {
 
+  temporadas: any[] = [];
 
+  constructor(private temporadasService: TemporadasServicesService) {}
 
-export class TemporadasComponentComponent {
-
-  
-
-
-  constructor(private temporadasService: TemporadasServicesService){
-
-  }
-
-  temporadas: any;
-
-  getTemporadas(){
-    
-    this.temporadasService.getTemporadas().subscribe((Response:any) => {
-      this.temporadas = Response.data;
-
-      console.log(this.temporadas); 
-    })
-
-  }
-
-  ngOnInit(){
+  ngOnInit(): void {
     this.getTemporadas();
   }
 
+  getTemporadas(): void {
+    this.temporadasService.getTemporadas().subscribe((Response: any) => {
+      this.temporadas = Response.data;
+    });
+  }
 }
